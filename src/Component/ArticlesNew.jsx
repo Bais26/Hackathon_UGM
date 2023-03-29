0443cf1f5bc1d0c6c098e011db1d7549735a806f
muhammadffa <muhammaddaffa463@gmail.com>
2023-03-29 10:09:58 +0700

    card articles

--- src/Component/ArticlesNew.jsx
+++ src/Component/ArticlesNew.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function ArticlesNew() {
     return (
@@ -9,26 +9,44 @@ export default function ArticlesNew() {
                     <p class="lg:pl-6 lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom.</p>
                 </div>
                 <div class="flex flex-wrap md:-m-2 -m-1">
-                    <div class="flex flex-wrap w-1/2">
-                        <div class="md:p-2 p-1 w-1/2">
-                            <img alt="gallery" class="w-full object-cover h-full object-center block" src="https://asset.kompas.com/crops/FenKB9Q5sBss57PZJ42JXPEVNUQ=/83x0:673x393/230x152/data/photo/2023/03/26/641f7bdded183.jpg" />
+                    <div class="flex flex-wrap w-1/2 ">
+                        <div class="md:p-2 p-1 w-1/2 transition-all hover:scale-105 group">
+                            <img alt="gallery" class="w-full group-hover:opacity-50 object-cover h-full object-center block" src="https://asset.kompas.com/crops/FenKB9Q5sBss57PZJ42JXPEVNUQ=/83x0:673x393/230x152/data/photo/2023/03/26/641f7bdded183.jpg" />
+                            <div className='px-5 fixed -mt-16 transition-all transform translate-y-8 opacity-0 group-hover:opacity-100 group-hover:translate-y-0 group-hover:absolute'>
+                                    <h1 class="text-2xl md:text-3xl text-black font-bold">TITLE HERE</h1>
+                            </div>
                         </div>
-                        <div class="md:p-2 p-1 w-1/2">
-                            <img alt="gallery" class="w-full object-cover h-full object-center block" src="https://asset.kompas.com/crops/buH9V8-l83otSdL6bpiAXpWkjxQ=/0x0:1500x1000/230x152/data/photo/2022/12/30/63ae8e9ea1cc4.jpeg" />
+                        <div class="md:p-2 p-1 w-1/2 transition-all hover:scale-105 group">
+                            <img alt="gallery" class="w-full group-hover:opacity-50 object-cover h-full object-center block" src="https://asset.kompas.com/crops/buH9V8-l83otSdL6bpiAXpWkjxQ=/0x0:1500x1000/230x152/data/photo/2022/12/30/63ae8e9ea1cc4.jpeg" />
+                            <div className='px-5 fixed -mt-16 transition-all transform translate-y-8 opacity-0 group-hover:opacity-100 group-hover:translate-y-0 group-hover:absolute'>
+                                <h1 class="text-2xl md:text-3xl text-black font-bold">TITLE HERE</h1>
+                            </div>
                         </div>
-                        <div class="md:p-2 p-1 w-full">
+                        <div class="md:p-2 p-1 w-full transform transition-all hover:scale-105 group">
                             <img alt="gallery" class="w-full h-full object-cover object-center block" src="https://asset.kompas.com/crops/ockloULVGrBxIV0V0z4VX7bee5s=/109x0:784x450/230x152/data/photo/2023/03/25/641e4290663ab.jpg" />
+                            <div className='px-5 fixed -mt-16 transition-all transform translate-y-8 opacity-0 group-hover:opacity-100 group-hover:translate-y-0 group-hover:absolute'>
+                                <h1 class="text-2xl md:text-3xl text-black font-bold">TITLE HERE</h1>
+                            </div>
                         </div>
                     </div>
                     <div class="flex flex-wrap w-1/2">
-                        <div class="md:p-2 p-1 w-full">
+                        <div class="md:p-2 p-1 w-full transform transition-all hover:scale-105 group">
                             <img alt="gallery" class="w-full h-full object-cover object-center block" src="https://dummyimage.com/601x361" />
+                            <div className='px-5 fixed -mt-16 transition-all transform translate-y-8 opacity-0 group-hover:opacity-100 group-hover:translate-y-0 group-hover:absolute'>
+                                <h1 class="text-2xl md:text-3xl text-black font-bold">TITLE HERE</h1>
+                            </div>
                         </div>
-                        <div class="md:p-2 p-1 w-1/2">
+                        <div class="md:p-2 p-1 w-1/2 transform transition-all hover:scale-110 group">
                             <img alt="gallery" class="w-full object-cover h-full object-center block" src="https://dummyimage.com/502x302" />
+                            <div className='px-5 fixed -mt-16 transition-all transform translate-y-8 opacity-0 group-hover:opacity-100 group-hover:translate-y-0 group-hover:absolute'>
+                                <h1 class="text-2xl md:text-3xl text-black font-bold">TITLE HERE</h1>
+                            </div>
                         </div>
-                        <div class="md:p-2 p-1 w-1/2">
+                        <div class="md:p-2 p-1 w-1/2 transform transition-all hover:scale-110 group">
                             <img alt="gallery" class="w-full object-cover h-full object-center block" src="https://dummyimage.com/503x303" />
+                            <div className='px-5 fixed -mt-16 transition-all transform translate-y-8 opacity-0 group-hover:opacity-100 group-hover:translate-y-0 group-hover:absolute'>
+                                <h1 class="text-2xl md:text-3xl text-black font-bold">TITLE HERE</h1>
+                            </div>
                         </div>
                     </div>
                 </div>
